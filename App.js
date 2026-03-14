37f5cafd2f276bd1c6dcc61910d8c8cd573b476d
Pabonfelipe2025 <pabonfelipe22@gmail.com>
2026-03-13 19:26:55 -0500

    Update redirect URI for login function

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 function login(){
 
 const client_key = "aw5hxji3xptu7r10";
-const redirect_uri = "https://pabonfelipe22-eng.github.io/Autoclipperalfaone/";
-
+const redirect_uri = "https://pabonfelipe22-eng.github.io/Autoclipperalfaone/callback";
 const scope = "user.info.basic,video.upload,video.publish";
 
 const url = `https://www.tiktok.com/v2/auth/authorize/
